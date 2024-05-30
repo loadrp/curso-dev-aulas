19c9979fa98a1314b464f7a631318c7cebdc1400
loadrp <lucas.rodrigues.prado120@gmail.com>
2024-05-30 13:47:59 -0300

    setuup migrations endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,28 +1,34 @@
-import database from "infra/database.js";
+import database from "infra/database";
 
 async function status(request, response) {
-  const databaseVersionQuery = await database.query("SHOW server_version;");
-  const databaseVersion = databaseVersionQuery.rows[0].server_version;
-
-  const maxConnections = await database.query('SHOW max_connections;')
-  const maxConnectionsValue = maxConnections.rows[0].max_connections;
   const updatedAt = new Date().toISOString();
- 
-  const databaseUsedConnectionsQuery = await database.query("SELECT count(*)::int FROM pg_stat_activity WHERE datname = 'local_db';")
-  const databaseUsedConnectionsValue = databaseUsedConnectionsQuery.rows[0].count
 
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  const databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections;"
+  );
+  const databaseMaxConnectionsValue =
+    databaseMaxConnectionsResult.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  const databaseOpenedConnectionsValue =
+    databaseOpenedConnectionsResult.rows[0].count;
 
   response.status(200).json({
     updated_at: updatedAt,
     dependencies: {
-      database:{
-        version: databaseVersion,
-        max_connections: parseInt(maxConnectionsValue),
-        opened_connections: databaseUsedConnectionsValue
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxConnectionsValue),
+        opened_connections: databaseOpenedConnectionsValue,
       },
-
-    }
-
+    },
   });
 }
 
